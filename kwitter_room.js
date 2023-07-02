b7cf5ed7371b99c8eb3ae503cf37bcb244b7fb78
Ay5sh1234 <135538891+Ay5sh1234@users.noreply.github.com>
2023-07-02 12:40:35 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,16 @@
 
-//ADD YOUR FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+      apiKey: "AIzaSyBJEUFxKMCdP5iupltqsnm7QL2HyEBZDg8",
+      authDomain: "chatting-643ae.firebaseapp.com",
+      databaseURL: "https://chatting-643ae-default-rtdb.firebaseio.com",
+      projectId: "chatting-643ae",
+      storageBucket: "chatting-643ae.appspot.com",
+      messagingSenderId: "312550617777",
+      appId: "1:312550617777:web:ef1bdc6aa61ed3b0376890",
+      measurementId: "G-ZKEDMNXBBH"
+    };
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
@@ -8,3 +19,20 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End code
       });});}
 getData();
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "kwitter_room.html";
+}
+
+function send()
+{
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+      });
+      document.getElementById("msg").value = "";
+}
